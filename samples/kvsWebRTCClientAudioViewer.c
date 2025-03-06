c4c3b6dcda32d4482e9dd901c7082c0b50a7c473
Toshio Yamada <mon.toshio.yamada@gmail.com>
2025-03-06 16:17:38 +0900

    sigHandler

--- samples/kvsWebRTCClientAudioViewer.c
+++ samples/kvsWebRTCClientAudioViewer.c
@@ -242,7 +242,7 @@ void* playbackThread(void* arg) {
 
 // SIGINT (Ctrl + C) で呼ばれるハンドラ (元サンプルが用意している想定)
 #ifndef _WIN32
-static void sigintHandler(int signum)
+void sigintHandler(int signum)
 {
     UNUSED_PARAM(signum);
     ATOMIC_STORE_BOOL(&gSampleConfiguration->interrupted, TRUE);
